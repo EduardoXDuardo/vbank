2d5f223f28905e4b30fe46dd11645890d763011f
EduardoXDuardo <luiz.e.silva@usp.br>
2025-08-21 12:04:15 -0300

    feat(accounts): Add paginated account findAll endpoint
    - Introduced a new GET endpoint in AccountController to retrieve accounts with pagination support using Pageable.
    
    - Added corresponding findAll method in AccountService to return a paginated list of AccountResponseDTOs.
    
    - Changed names of the method account findById for consistency.

--- src/main/java/com/eduardoxduardo/vbank/controller/AccountController.java
+++ src/main/java/com/eduardoxduardo/vbank/controller/AccountController.java
@@ -3,6 +3,8 @@ package com.eduardoxduardo.vbank.controller;
 import com.eduardoxduardo.vbank.dto.AccountResponseDTO;
 import com.eduardoxduardo.vbank.service.AccountService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,8 +19,14 @@ public class AccountController {
     private final AccountService accountService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<AccountResponseDTO> getAccountById(@PathVariable Long id) {
+    public ResponseEntity<AccountResponseDTO> findById(@PathVariable Long id) {
         AccountResponseDTO accountResponse = accountService.findById(id);
         return ResponseEntity.ok(accountResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<AccountResponseDTO>> findAll(Pageable pageable) {
+        Page<AccountResponseDTO> accountsPage = accountService.findAll(pageable);
+        return ResponseEntity.ok(accountsPage);
     }
 }
--- src/main/java/com/eduardoxduardo/vbank/service/AccountService.java
+++ src/main/java/com/eduardoxduardo/vbank/service/AccountService.java
@@ -5,6 +5,8 @@ import com.eduardoxduardo.vbank.mapper.AccountMapper;
 import com.eduardoxduardo.vbank.model.entities.Account;
 import com.eduardoxduardo.vbank.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,5 +23,12 @@ public class AccountService {
         Account account = accountRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Account not found"));
         return AccountMapper.toDTO(account);
+    }
+
+    // TODO: Change the method from findAll to search and implement filtering and sorting functionality
+    @Transactional(readOnly = true)
+    public Page<AccountResponseDTO> findAll(Pageable pageable) {
+        Page<Account> accountsPage = accountRepository.findAll(pageable);
+        return accountsPage.map(AccountMapper::toDTO);
     }
 }
