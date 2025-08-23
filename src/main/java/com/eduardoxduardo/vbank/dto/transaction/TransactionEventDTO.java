9b345f14726d6611532e7e6ece100ce415e6d814
EduardoXDuardo <luiz.e.silva@usp.br>
2025-08-23 00:31:25 -0300

    feat(transactions): Implement async transaction request with RabbitMQ
    - Introduces TransactionEventDTO and refactors TransactionService to support asynchronous transaction processing. The service now creates a pending transaction and publishes an event to RabbitMQ for further processing, replacing the previous synchronous logic.

---
+++ src/main/java/com/eduardoxduardo/vbank/dto/transaction/TransactionEventDTO.java
@@ -0,0 +1,14 @@
+package com.eduardoxduardo.vbank.dto.transaction;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransactionEventDTO {
+    private Long transactionId;
+}
